6bf72ac8767cb3df85b41b7f00b3e36fd2875206
AbbAlgNor <algot.nordstrom@abbindustrigymnasium.se>
2021-09-03 15:50:42 +0200

    added footer and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navbar />
+  <div class="spacer"></div>
+  <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Footer,
+    Navbar,
+  },
+};
 </script>
 
 <style>
@@ -20,7 +23,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+body {
+  margin: 0;
+}
+.spacer {
+  min-height: 500px;
 }
 </style>
