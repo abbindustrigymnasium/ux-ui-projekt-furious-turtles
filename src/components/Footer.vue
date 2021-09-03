07df9826d0658b595a6ddd17d3f506e72de9fa48
AbbAlgNor <algot.nordstrom@abbindustrigymnasium.se>
2021-09-03 15:50:53 +0200

    added footer and navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="footerContainer">
+    <div class="footer">
+      <div class="footerCollum">
+        <h3>Shop</h3>
+        <p>RazerCookies</p>
+        <p>Cake Locator</p>
+        <p>Puchase Programs</p>
+        <p>Exclusive Muffins</p>
+        <p>Last Chance Doughnuts!</p>
+        <p>Newsletter</p>
+      </div>
+
+      <div class="footerCollum">
+        <h3>Explore</h3>
+        <p>Cookie Technologies</p>
+        <p>RGB Cakes</p>
+        <p>Conceps</p>
+        <p>Esport Doughnuts</p>
+        <p>Collabs</p>
+      </div>
+
+      <div class="footerCollum">
+        <h3>Support</h3>
+        <p>Warranty</p>
+        <p>CookieCare</p>
+        <p>Manage Cake ID</p>
+      </div>
+      <div class="footerCollum">
+        <h3>Company</h3>
+        <p>About Us</p>
+        <p>Careers</p>
+        <p>Press Room</p>
+        <p>Investor Relations</p>
+        <p>Contact Us</p>
+      </div>
+      <div class="footerCollum" id="forGamers">
+        <h3>FOR GAMERS. BY GAMERS.™</h3>
+        <i class="fa fa-twitter fa-2x"></i>
+        <i class="fab fa-instagram fa-2x"></i>
+        <i class="fab fa-facebook fa-2x"></i>
+      </div>
+    </div>
+    <div class="footerFooter">
+      <div class="left">
+        <p>Copyright © 2021 Furious Turtles Inc. All rights reserved.</p>
+        <p>|</p>
+        <p>Site Map</p>
+        <p>|</p>
+        <p>Legal Terms</p>
+        <p>|</p>
+        <p>Privacy Policy</p>
+        <p>|</p>
+        <p>Cookie Policy</p>
+      </div>
+      <p>Sweden</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.footerContainer {
+  width: 100%;
+  margin: none;
+  padding: none;
+  background-color: #111111;
+  padding-left: 100px;
+  padding-right: 100px;
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px black dotted;
+  min-width: 700px;
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.footerCollum {
+  text-align: left;
+  margin-top: 0px;
+  margin-bottom: auto;
+}
+.left {
+  display: flex;
+  justify-content: space-between;
+  margin-left: auto;
+  min-width: 700px;
+  max-width: 1000px;
+  margin-right: auto;
+}
+p {
+  color: #8a8a8a;
+}
+p:hover {
+  color: white;
+  text-decoration: underline;
+  cursor: pointer;
+}
+h3 {
+  color: #ededed;
+}
+#forGamers,
+#forGamers h3 {
+  color: lime;
+  text-align: right;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="navbarContainer">
+    <div class="navbar">
+      <img
+        class="navitem"
+        src="../../public/razer-ths-logo.svg"
+        alt="razer Logo"
+        width="50"
+      />
+      <h3 class="navitem">Shop</h3>
+      <h3 class="navitem">Featured</h3>
+      <i class="fas fa-search navitem"></i>
+      <i class="fas fa-shopping-cart navitem"></i>
+      <i class="fas fa-user-circle navitem"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+.navbarContainer {
+  background-color: #111111;
+  color: #8a8a8a;
+  padding-left: 100px;
+  padding-right: 100px;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 0;
+  margin-bottom: auto;
+  border: black 2px dotted;
+}
+.navitem {
+  margin-top: auto;
+  margin-bottom: auto;
+}
+</style>
